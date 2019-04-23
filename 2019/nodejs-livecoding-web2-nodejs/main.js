2b250c0d3fa482a19a4905d7d2cbb3d4d297b04b
Steve Jeong <yyie.jeong@samsung.com>
2019-04-23 17:48:35 +0900

    add readdir.js

--- 2019/nodejs-livecoding-web2-nodejs/main.js
+++ 2019/nodejs-livecoding-web2-nodejs/main.js
@@ -12,53 +12,70 @@ const app = http.createServer(function (request, response) {
       let title = 'Welcome';
       let description = 'Welcome, Node.js';
 
-      const template = `
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        <ul>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>
-        <h2>${title}</h2>
-        <p>${description}
-        </p>
-      </body>
-      </html>
-      `;
-      response.writeHead(200);
-      response.end(template);
+      fs.readdir('./data', (err, files) => {
+
+        let list = '<ul>';
+
+        let i = 0;
+        while(i < files.length) {
+          list += `<li><a href="/?id=${files[i]}">${files[i]}</a></li>`;
+          i = i + 1;
+        }
+        list += '</ul>';
+
+        const template = `
+        <!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          ${list}
+          <h2>${title}</h2>
+          <p>${description}
+          </p>
+        </body>
+        </html>
+        `;
+        response.writeHead(200);
+        response.end(template);
+      });
     } else {
       fs.readFile(`data/${queryData.id}`, 'utf-8', (err, description) => {
         let title = queryData.id;
-        const template = `
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        <ul>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>
-        <h2>${title}</h2>
-        <p>${description}
-        </p>
-      </body>
-      </html>
-      `;
-        response.writeHead(200);
-        response.end(template);
+
+        fs.readdir('./data', (err, files) => {
+
+          let list = '<ul>';
+
+          let i = 0;
+          while (i < files.length) {
+            list += `<li><a href="/?id=${files[i]}">${files[i]}</a></li>`;
+            i = i + 1;
+          }
+          list += '</ul>';
+
+          const template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            ${list}
+            <h2>${title}</h2>
+            <p>${description}
+            </p>
+          </body>
+          </html>
+          `;
+          response.writeHead(200);
+          response.end(template);
+        });
       });
     }
   } else {
