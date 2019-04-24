15c560ae3965bd0df0f1719eb63e7ab1535c7c16
Steve Jeong <yyie.jeong@samsung.com>
2019-04-24 11:14:41 +0900

    add function.js function2.js and modify main.js

--- 2019/nodejs-livecoding-web2-nodejs/main.js
+++ 2019/nodejs-livecoding-web2-nodejs/main.js
@@ -2,6 +2,34 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 
+const templateHTML = (title, list, body) => {
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}    
+  </body>
+  </html>
+  `;
+};
+
+const templateList = (files) => {
+  let list = '<ul>';
+  let i = 0;
+  while(i < files.length) {
+    list += `<li><a href="/?id=${files[i]}">${files[i]}</a></li>`;
+    i = i + 1;
+  }
+  list += '</ul>';
+  return list;
+};
+
 const app = http.createServer(function (request, response) {
   let _url = request.url;
   const queryData = url.parse(_url, true).query;
@@ -9,70 +37,20 @@ const app = http.createServer(function (request, response) {
 
   if (pathname === '/') {
     if (queryData.id === undefined) {
-      let title = 'Welcome';
-      let description = 'Welcome, Node.js';
-
+      const title = 'Welcome';
+      const description = 'Welcome, Node.js';
       fs.readdir('./data', (err, files) => {
-
-        let list = '<ul>';
-
-        let i = 0;
-        while(i < files.length) {
-          list += `<li><a href="/?id=${files[i]}">${files[i]}</a></li>`;
-          i = i + 1;
-        }
-        list += '</ul>';
-
-        const template = `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          ${list}
-          <h2>${title}</h2>
-          <p>${description}
-          </p>
-        </body>
-        </html>
-        `;
+        const list = templateList(files);
+        const template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
         response.writeHead(200);
         response.end(template);
       });
     } else {
       fs.readFile(`data/${queryData.id}`, 'utf-8', (err, description) => {
-        let title = queryData.id;
-
         fs.readdir('./data', (err, files) => {
-
-          let list = '<ul>';
-
-          let i = 0;
-          while (i < files.length) {
-            list += `<li><a href="/?id=${files[i]}">${files[i]}</a></li>`;
-            i = i + 1;
-          }
-          list += '</ul>';
-
-          const template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            ${list}
-            <h2>${title}</h2>
-            <p>${description}
-            </p>
-          </body>
-          </html>
-          `;
+          const title = queryData.id;
+          const list = templateList(files);
+          const template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
           response.writeHead(200);
           response.end(template);
         });
---
+++ 2019/nodejs-livecoding-web2-nodejs/syntax/function.js
@@ -0,0 +1,24 @@
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(1);
+console.log(2);
+console.log(3);
+console.log('#######');
+f123();
+f123();
+
+function f123() {
+  console.log(1);
+  console.log(2);
+  console.log(3);
+}
