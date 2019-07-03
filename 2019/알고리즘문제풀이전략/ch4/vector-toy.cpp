9e423bc74204e22c0f36720f13e70d935c2c9a8b
Steve Jeong <singular1.steve@gmail.com>
2019-07-03 18:10:16 +0900

    add cpp vector toy

--- 2019/알고리즘문제풀이전략/ch4/vector-toy.cpp
+++ 2019/알고리즘문제풀이전략/ch4/vector-toy.cpp
@@ -3,3 +3,30 @@ cpp vector test
 
 ref: https://blockdmask.tistory.com/70
  */
+
+#include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+// 소인수분해
+vector<int> factor(int n) {
+  if(n==1) return vector<int>(1, 1);
+  vector<int> ret;
+  for(int div = 2; n > 1; div++) {
+    while(n % div == 0) {
+      n /= div;
+      ret.push_back(div);
+    }
+  }
+  return ret;
+}
+
+int main() {
+  int n;
+  scanf("%d", &n);
+  vector<int> v = factor(n);
+
+  
+  return 1;
+}
