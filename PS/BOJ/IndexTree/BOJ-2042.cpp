ee038a59a8e7476134fb4ffec7bab087ab745586
unknown <singular1.steve@gmail.com>
2019-08-22 18:07:06 +0900

    solve boj-2042

---
+++ PS/BOJ/IndexTree/BOJ-2042.cpp
@@ -0,0 +1,56 @@
+/*
+BOJ 2042 구간합구하기
+
+*/
+#include <stdio.h>
+
+int tree[1000000 * 4];
+
+void update(int node, int start, int end, int left, int right, int delta) {
+  printf("update node[%d] : %d ~ %d %d ~ %d\n", node, start, end, left, right);
+  if(left > end || right < start) return;
+  
+  if(left <= start && end <= right) {
+    tree[node] += delta;
+    // printf("node[%d] updated \n", node);
+    return;
+  }
+  int mid = (start + end) / 2;
+  update(node * 2, start, mid, left, right, delta);
+  update(node * 2 + 1, mid + 1, end, left, right, delta);
+}
+
+long long sum(int node, int start, int end, int left, int right) {
+  if(left > end || right < start) return 0;
+  if(left <= start && end <= right) return tree[node];
+  int mid = (start + end) / 2;
+  return sum(node * 2, start, mid, left, right) + sum(node * 2 + 1, mid + 1, end, left, right);
+}
+
+int main() {
+  int n, m, k;
+  int a, b, c;
+  scanf("%d %d %d", &n, &m, &k);
+
+  int idx = 2;
+  while(idx < n) {
+    idx = idx << 1;
+  }
+
+  for(int i = idx; i <= idx + n - 1; i++) {
+    scanf("%d", &tree[i]);
+  }
+  for(int i = idx - 1; i >= 1; i--) {
+    tree[i] = tree[i * 2] + tree[i * 2 + 1];
+  }
+
+  for(int i = 1; i <= m + k; i++) {
+    scanf("%d %d %d", &a, &b, &c);
+    if(a == 1) {
+      update(1, 1, idx, b, b, c- tree[b]);
+    } else {
+      printf("%d\n", sum(1, 1, idx, b, c));
+    }
+  }
+  return 0;
+}
